e05e0d8ab3e5caed7c32da893a10b621ab4f4373
nanofishie <nanofishie@gmail.com>
2015-04-09 02:08:57 -0400

    Removed debugging prompt from hello world

--- VisualCSharpBook/01/TestHello/TestHello/Program.cs
+++ VisualCSharpBook/01/TestHello/TestHello/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +14,8 @@ namespace TestHello
             Console.Write("{0}Hello World!{0}{0}Please enter your name:  ",System.Environment.NewLine);
             string userName = Console.ReadLine();
             DateTime dt = System.DateTime.Today;
-            Console.WriteLine(String.Format("{2}Hello, {0}! Today is {1:MM/dd/yyyy}.{2}{2}Press enter to exit", userName, dt.Date, System.Environment.NewLine));
+            Console.WriteLine(String.Format("{2}Hello, {0}! Today is {1:MM/dd/yyyy}.", userName, dt.Date, System.Environment.NewLine));
+            // Debug.WriteLine(String.Format("{0}Press enter to exit.", System.Environment.NewLine));
             Console.ReadLine();
         }
     }
