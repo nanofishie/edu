a009d334c87babfde448e2532e47bb8b9d5fc7d8
nanofishie <nanofishie@gmail.com>
2015-04-08 23:15:46 -0400

    Hello world console with name and date

--- VisualCSharpBook/01/TestHello/TestHello/Program.cs
+++ VisualCSharpBook/01/TestHello/TestHello/Program.cs
@@ -10,6 +10,11 @@ namespace TestHello
     {
         static void Main(string[] args)
         {
+            Console.Write("{0}Hello World!{0}{0}Please enter your name:  ",System.Environment.NewLine);
+            string userName = Console.ReadLine();
+            DateTime dt = System.DateTime.Today;
+            Console.WriteLine(String.Format("{2}Hello, {0}! Today is {1:MM/dd/yyyy}.{2}{2}Press enter to exit", userName, dt.Date, System.Environment.NewLine));
+            Console.ReadLine();
         }
     }
 }
